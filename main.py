d191481106de372c558301c3f3c42fb17fbe5998
avylash95 <abhilashmohanty86@gmail.com>
2026-09-23 16:31:39 +0530

    agent working and tested

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from src.tools.tools import web_search, scrape_url
+from src.pipeline.pipeline import run_research_pipeline
 
-result = web_search.invoke("What is the latest research on using AI for climate change mitigation?")
-print(result)
+#result = web_search.invoke("What is the latest research on using AI for climate change mitigation?")
+#print(result)
+
+topic = "What is the latest research on using AI for climate change mitigation?"
+run_research_pipeline(topic)
